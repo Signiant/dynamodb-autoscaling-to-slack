b44fad4f60821e33ef265534cc5b936b225bf168
Dave North <dnorth@signiant.com>
2017-09-19 11:31:18 -0400

    Add travis build config

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-shopt -s extglob 
 
 mkdir -p dist
-pip install slacker -t ./dist
 cp -f lambda/dynamodb-as-notify-slack.py ./dist/dynamodb-as-notify-slack.py
-pushd .
 cd dist
 zip -r dynamodb-as-notify-slack .
-popd 
+cd ..
