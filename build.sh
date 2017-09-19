ec2e75dc591900a94b4c272061c79246854c9ca1
Dave North <dnorth@signiant.com>
2017-09-19 11:51:32 -0400

    Add some echos

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Creating dist folder"
 mkdir -p dist
+echo "Copying lambda code into dist folder"
 cp -f lambda/dynamodb-as-notify-slack.py ./dist/dynamodb-as-notify-slack.py
 cd dist
+echo "Creating lambda zip"
 zip -r dynamodb-as-notify-slack .
 cd ..
+echo "DONE"
